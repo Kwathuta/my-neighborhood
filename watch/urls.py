1028ad57a1257dfa14d3b194c28a2305922bed39
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-11-03 02:30:55 +0300

    optimizing routes

--- watch/urls.py
+++ watch/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("profile", views.index, name="index"),
+    path("profile/", views.profile, name="index"),
     path("", views.profile, name="profile"),
     path("profile/update", views.update_profile, name="update_profile"),
     path("posts", views.posts, name='posts'),
